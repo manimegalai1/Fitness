db9fa2a7ccd834f954a6c979d2d4499690b4b258
Manimega <manimega181@gmail.com>
2024-12-20 16:57:25 +0800

    Initial Commit

---
+++ Fitness/FitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessApp.swift
+//  Fitness
+//
+//  Created by Apple on 20/12/24.
+//
+
+import SwiftUI
+
+@main
+struct FitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
